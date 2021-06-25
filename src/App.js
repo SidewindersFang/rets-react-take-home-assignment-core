92941db69d4293fb731c55792e0b361c141f4ada
SidewindersFang <auggiehudak@gmail.com>
2021-06-25 14:51:05 -0700

    add StickyHeader

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-/**
- * Remove this content and start here
- */
-
+import { StickyHeader } from 'components/StickyHeader';
 import heartFill from './assets/heart-fill.svg';
 import heartStroke from './assets/heart-stroke.svg';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <div
-      className={styles.app}
-    >
-      <h1>Side React Take-home Assignment</h1>
-      <p>
-        Welcome to the Side React take-home assignment. This boilerplate is
-        intended to help get you started. It's already setup with the fonts and
-        SVG assets you will need to complete your assignment.
-      </p>
-      <p>
-        <em>
-          Be sure to read through the INSTRUCTIONS.md content before beginning.
-          Good luck!
-        </em>
-      </p>
-      <div>
-        <div>Here are the SVG assets you will need.</div>
-        <img src={heartFill} className="App-logo" alt="favorite icon" />
-        <img src={heartStroke} className="App-logo" alt="unfavorite icon" />
-      </div>
+    <div className={styles.app}>
+      <StickyHeader />
     </div>
   );
 }
---
+++ src/components/StickyHeader.jsx
@@ -0,0 +1,7 @@
+import styles from './StickyHeader.module.css';
+
+const StickyHeader = () => {
+    return (<div className={styles.header}><p>Property Listings</p></div>)
+};
+
+export { StickyHeader };
