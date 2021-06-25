1197524a9058fce6ec18a6a7750fbd92c5217d32
SidewindersFang <auggiehudak@gmail.com>
2021-06-25 14:21:02 -0700

    refactor to css modules

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@
 
 import heartFill from './assets/heart-fill.svg';
 import heartStroke from './assets/heart-stroke.svg';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div
-      style={{
-        maxWidth: '70%',
-        padding: '1em',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        background: '#eee',
-      }}
+      className={styles.app}
     >
       <h1>Side React Take-home Assignment</h1>
       <p>
